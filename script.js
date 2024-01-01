cc8c902a5a429bd1ba71e0e6db3ad42ffc19f70a
Praharsh Patel <53941184+prxhxrsh@users.noreply.github.com>
2024-01-01 14:01:48 -0500

    done

--- script.js
+++ script.js
@@ -4,13 +4,23 @@ rectangle.addEventListener("mousemove",function(details){
         var rectangleLocation = rectangle.getBoundingClientRect(); 
         var insideRectLeft = details.clientX- rectangleLocation.left
           if (insideRectLeft <rectangleLocation.width/2 )  {
-            rectangle.style.backgroundColor = "red";
+           var redColor=  gsap.utils.mapRange(0,rectangleLocation.width/2,255,0,insideRectLeft)
+            gsap.to(rectangle,{
+                backgroundColor: `rgb(${redColor},0,0)`,
+                ease:Power4
+            })
           }else{
-            rectangle.style.backgroundColor = "Blue";
-          }
-            
-})
+            var greenColor=  gsap.utils.mapRange(rectangleLocation.width/2,rectangleLocation.width,0,255,insideRectLeft)
+            gsap.to(rectangle,{
+                backgroundColor: `rgb(0,${greenColor},0)`,
+                ease:Power4
+            })
+        }   
+    })
+
 
 rectangle.addEventListener("mouseleave",function(){
     rectangle.style.backgroundColor = "white";
 })
+
+
